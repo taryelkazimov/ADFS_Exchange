88a8c18a49625448c5476a2ce7c34b26f21e37c9
taryelkazimov <35764132+taryelkazimov@users.noreply.github.com>
2018-09-18 10:07:05 +0400

    RTM Version

---
+++ Search-MailboxDeleteContent.ps1
@@ -0,0 +1,6 @@
+﻿Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+$mailbox = Get-Content "C:\SCRIPTS\SearchMailboxDeleteContent\mailbox.txt"
+foreach($box in $mailbox)
+    {
+        Search-Mailbox -Identity $box -DeleteContent -Force
+    }
